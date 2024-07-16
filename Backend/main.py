29d6ec64c7a01a56ece5fb88b7bf91ed33a0411d
sharvsave1023 <82337331+sharvsave1023@users.noreply.github.com>
2024-07-15 19:30:05 -0500

    login page linked! styling not being applied

--- Backend/main.py
+++ Backend/main.py
@@ -4,12 +4,8 @@ from flask_cors import CORS
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/')
-def home():
-    return "Welcome to FinTech UTD's Spring 2024 Project: Robo-Advisor Platform"
-
-@app.route('/loginPage')
 def login():
-    return render_template("index.html")
+    return render_template("Frontend/robo-advisor/public/index.html")
 
 @app.route('/register')
 def register():
