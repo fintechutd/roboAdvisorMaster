ac152525f347d591e205527f40202ea0fda86322
sourish pasula <sourishpasula@sourishs-MBP.attlocal.net>
2024-07-30 19:01:33 -0500

    questionnaire added and dashboard added but css is funky on dashboard.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,6 +4,7 @@ import Login from './components/Login'
 import Logo from './components/Logo'
 import './index.css'
 import Questionnaire from './components/Questionnaire/Questionnaire'
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
 
@@ -11,7 +12,9 @@ function App() {
     <Router>
       <Routes>
         
-        <Route path='/' element={<Questionnaire/>}></Route>
+        <Route path='/questionnaire' element={<Questionnaire/>}></Route>
+        <Route path='/dashboard' element={<Dashboard/>}></Route>
+
       </Routes>
     </Router>
   )
